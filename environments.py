7b5232182c928beffec9be3034f68f3364482cee
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-09-29 21:03:05 +0200

    Added option to play yourself

--- environments.py
+++ environments.py
@@ -8,6 +8,7 @@ https://github.com/openai/procgen#environment-options
 class Environment:
     def __init__(self, render=False) -> None:
         self.render = render
+        self.interactive = True
         self.bigfish = 'procgen:procgen-bigfish-v0'
         self.bossfight = 'procgen:procgen-bossfight-v0'
         self.caveflyer = 'procgen:procgen-caveflyer-v0'
@@ -26,6 +27,7 @@ class Environment:
         self.starpilot = 'procgen:procgen-starpilot-v0'
 
     def __getattribute__(self, name: str) -> str:
+
         evn_name = object.__getattribute__(self, name)
         if object.__getattribute__(self, 'render'):
             return gym.make(evn_name, render_mode="human")
@@ -38,3 +40,16 @@ class Environment:
             return gym.make(evn_name, render_mode="human")
         else:
             return gym.make(evn_name)
+
+
+if __name__ == "__main__":
+    from procgen.interactive import make_interactive, ENV_NAMES
+    print(ENV_NAMES)
+    kwargs = {}
+    kwargs["distribution_mode"] = "hard"
+    kwargs["start_level"] = 0
+    kwargs["num_levels"] = 0
+    ia = make_interactive(
+        "human", record_dir=None, env_name=input(), **kwargs
+    )
+    ia.run()
