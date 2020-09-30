608c0d93625f0227778d1651c56d4e02a3f1fac4
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-09-30 20:40:16 +0200

    Created replay buffer

--- agent.py
+++ agent.py
@@ -1,14 +1,36 @@
 """
-https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+[1] https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+[2] https://www.element61.be/en/resource/quick-guide-reinforcement-learning-and-how-experiment-using-openai-gym
 """
-# Example network from week 5
+
+from collections import namedtuple
 import torch
+import random
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
 
-class Agent(nn.Module):
+class Agent:
+    def __init__(self) -> None:
+        self.network = NetWork()
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(self.network.parameters(), lr=3e-4, weight_decay=1e-5)
+        self.memory = ReplayMemory(1000)
+
+    def choose(self, obs):
+        result = NetWork(obs)
+        return "action"
+
+    def learn(self):
+        pass
+
+    def remember(self, state, action, next_state, reward, done):
+        self.memory.push(state, action, next_state, reward)
+
+
+# Example network from week 5
+class NetWork(nn.Module):
     def __init__(self):
         super(Agent, self).__init__()
 
@@ -35,3 +57,28 @@ class Agent(nn.Module):
         x = self.l_out(x)
 
         return x
+
+
+# [1]
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    # [1]
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
