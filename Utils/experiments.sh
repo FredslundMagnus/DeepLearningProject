899fe918b3a669f4cbcf636fa9c873e2b9e6bf32
FredslundMagnus <s183905@student.dtu.dk>
2020-11-02 18:24:58 +0100

    tester

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mkdir ../outputs/Discount_0.70/
-mkdir ../outputs/Discount_0.70/Markdown
-bsub -o "../outputs/Discount_0.70/Markdown/Discount_0.70_0.md" -J "Discount_0.70_0" -P "Discount_0.70-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
+mkdir ../outputs/test_1/
+mkdir ../outputs/test_1/Markdown
+mkdir ../outputs/test_1/Agents
+bsub -o "../outputs/test_1/Markdown/test_1_0.md" -J "test_1_0" -P "test_1-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
