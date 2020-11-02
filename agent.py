233707f86459f29a10e4b25ab360d7ef5a3c0aed
FredslundMagnus <s183905@student.dtu.dk>
2020-11-02 15:34:23 +0100

    set_lr_lower

--- agent.py
+++ agent.py
@@ -17,7 +17,7 @@ class Agent:
         self.network = NetWork().to(device)
         print("Number of parameters in network:", count_parameters(self.network))
         self.criterion = MSELoss()
-        self.optimizer = Adam(self.network.parameters(), lr=1e-3, weight_decay=1e-5)
+        self.optimizer = Adam(self.network.parameters(), lr=1e-4, weight_decay=1e-5)
         self.memory = ReplayBuffer(100000)
         self.remember = self.memory.remember()
         self.exploration = Exploration()
