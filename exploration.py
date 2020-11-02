08553605b2e56b91c24716254bad8e73254fc5d5
FredslundMagnus <s183905@student.dtu.dk>
2020-11-02 14:11:22 +0100

    good_printer

--- exploration.py
+++ exploration.py
@@ -6,11 +6,11 @@ from numpy.random import choice
 
 class Exploration():
     def __init__(self) -> None:
-        self.K = 0.2
+        self.K = 0.05
         self.epsilon = 0.05
 
     def softmax(self, vals):
-        print(str(float(vals.max()))[:4], end=", ")
+        print(f"({str(float(vals.max()))[:4]}, {str(float(vals.var()))[:4]})", end=", ")
         # print(vals)
         return int(choice(15, 1, p=softmax(vals / self.K, dim=0).detach().cpu().numpy()))
 
@@ -18,5 +18,5 @@ class Exploration():
         return vals.detach().cpu().numpy().argmax()
 
     def epsilonGreedy(self, vals):
-        print(str(float(vals.max()))[:4], end=", ")
+        print(f"({str(float(vals.max()))[:4]}, {str(float(vals.var()))[:4]})", end=", ")
         return int(choice(15, 1)) if random() < self.epsilon else vals.detach().cpu().numpy().argmax()
