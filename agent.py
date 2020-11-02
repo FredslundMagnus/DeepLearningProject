231a9c728c8e430337e4e0636419965d06fa623e
FredslundMagnus <s183905@student.dtu.dk>
2020-11-02 13:39:04 +0100

    better_network

--- agent.py
+++ agent.py
@@ -60,40 +60,38 @@ class NetWork(Module):
 
         self.conv1 = Sequential(
             Conv2d(in_channels=3, out_channels=8, kernel_size=1),
-            Conv2d(in_channels=8, out_channels=16, kernel_size=4, stride=2),
+            Conv2d(in_channels=8, out_channels=16, kernel_size=8, stride=4),
             LeakyReLU(),
-            Conv2d(in_channels=16, out_channels=20, kernel_size=4, stride=2),
+            Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2),
             LeakyReLU(),
-            Conv2d(in_channels=20, out_channels=20, kernel_size=3),
-            LeakyReLU(),
-        )
-
-        self.conv2 = Sequential(
-            Conv2d(in_channels=15, out_channels=20, kernel_size=3),
-            LeakyReLU(),
-            Conv2d(in_channels=20, out_channels=25, kernel_size=3),
-            MaxPool2d(2, 2, padding=0),
+            Conv2d(in_channels=32, out_channels=32, kernel_size=3),
             LeakyReLU(),
         )
 
-        self.conv3 = Sequential(
-            Conv2d(in_channels=25, out_channels=20, kernel_size=1),
-            LeakyReLU(),
-            Conv2d(in_channels=20, out_channels=15, kernel_size=3),
-            # MaxPool2d(2, 2, padding=0),
-            LeakyReLU(),
-        )
+        # self.conv2 = Sequential(
+        #     Conv2d(in_channels=15, out_channels=20, kernel_size=3),
+        #     LeakyReLU(),
+        #     Conv2d(in_channels=20, out_channels=25, kernel_size=3),
+        #     MaxPool2d(2, 2, padding=0),
+        #     LeakyReLU(),
+        # )
+
+        # self.conv3 = Sequential(
+        #     Conv2d(in_channels=25, out_channels=20, kernel_size=1),
+        #     LeakyReLU(),
+        #     Conv2d(in_channels=20, out_channels=15, kernel_size=3),
+        #     # MaxPool2d(2, 2, padding=0),
+        #     LeakyReLU(),
+        # )
 
         # self.size_after_conv = 720
         # self.lstm = LSTM(self.size_after_conv, hidden_size, 2)
 
         self.linear = Sequential(
-            Linear(2880, 100),
+            Linear(512, 100),
             # Linear(hidden_size, 40),
             LeakyReLU(),
-            Linear(100, 80),
-            LeakyReLU(),
-            Linear(80, 15),
+            Linear(100, 15),
         )
 
     def forward(self, x):
@@ -104,7 +102,7 @@ class NetWork(Module):
         # x = x.view(1, -1, self.size_after_conv)
         # x, (self.hn, self.cn) = self.lstm(x, (self.hn, self.cn))
         # x = x.view(-1, hidden_size)
-        x = x.view(-1, 2880)
+        x = x.view(-1, 512)
         x = self.linear(x)
         return x
 
