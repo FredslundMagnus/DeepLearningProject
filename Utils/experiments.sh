1f13b8c0ceaada3887a2fa9b759921f198d53aad
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-11-02 19:37:18 +0100

    test

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir ../outputs/test_2/
-mkdir ../outputs/test_2/Markdown
-mkdir ../outputs/test_2/Agents
-bsub -o "../outputs/test_2/Markdown/test_2_0.md" -J "test_2_0" -P "test_2-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
+mkdir ../outputs/test_3/
+mkdir ../outputs/test_3/Markdown
+mkdir ../outputs/test_3/Agents
+bsub -o "../outputs/test_2/Markdown/test_3_0.md" -J "test_3_0" -P "test_3-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
--- Utils/submit.sh
+++ Utils/submit.sh
@@ -5,7 +5,7 @@
 #BSUB -n 1
 #BSUB -R "rusage[mem=20G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 10
+#BSUB -W 60
 # end of BSUB options
 cd ..
 module -s load python3
