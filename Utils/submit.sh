94ab7b8acbf6b27b4e6f2be302e3ba0edafc7d83
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-11-03 01:14:30 +0100

    test

--- Utils/submit.sh
+++ Utils/submit.sh
@@ -3,9 +3,9 @@
 #BSUB -q gpuv100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 1
-#BSUB -R "rusage[mem=20G]"
+#BSUB -R "rusage[mem=25G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 60
+#BSUB -W 1000
 # end of BSUB options
 cd ..
 module -s load python3
