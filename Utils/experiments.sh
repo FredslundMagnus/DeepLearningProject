ccf25de937f22e03530139392e5c2dccd32a6a78
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-11-03 01:13:43 +0100

    test

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mkdir ../outputs/test_3/
-mkdir ../outputs/test_3/Markdown
-mkdir ../outputs/test_3/Agents
-bsub -o "../outputs/test_2/Markdown/test_3_0.md" -J "test_3_0" -P "test_3-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
+mkdir ../outputs/test_4/
+mkdir ../outputs/test_4/Markdown
+mkdir ../outputs/test_4/Agents
+bsub -o "../outputs/test_4/Markdown/test_4_0.md" -J "test_4_0" -P "test_4-0 -lossf MME -discount 0.12 -lambd 0.3 -lr 0.0001 -dropout 0" < submit.sh
