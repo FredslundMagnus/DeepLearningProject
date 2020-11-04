237d3a4f5b536194ba9686dde04a4954f23a17bc
TheDingodile <TheDingodile@gmail.com>
2020-11-04 22:57:47 +0100

    lrandreplay

--- agent.py
+++ agent.py
@@ -17,8 +17,8 @@ class Agent:
         self.network = NetWork().to(device)
         print("Number of parameters in network:", count_parameters(self.network))
         self.criterion = MSELoss()
-        self.optimizer = Adam(self.network.parameters(), lr=1e-5, weight_decay=1e-5)
-        self.memory = ReplayBuffer(100000)
+        self.optimizer = Adam(self.network.parameters(), lr=1e-4, weight_decay=1e-5)
+        self.memory = ReplayBuffer(50000)
         self.remember = self.memory.remember()
         self.exploration = Exploration()
         self.explore = self.exploration.softmax
