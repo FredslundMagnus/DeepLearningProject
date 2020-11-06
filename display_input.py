aae846f80c2ed5074dab2f3c7325c1929362784f
FredslundMagnus <s183905@student.dtu.dk>
2020-11-06 16:01:46 +0100

    fix

--- display_input.py
+++ display_input.py
@@ -54,8 +54,8 @@ def parametres(agent: Agent, x: int = 1000, y: int = 0):
 def imageBig(state, x: int = 1000, y: int = 0):
     fig = plt.figure()
     move_figure(fig, x, y)
-    the_state = np.array(state).reshape(3, 64, 64).transpose(1, 2, 0)
-    plt.imshow(the_state / 255)
+    the_state = np.array(state).reshape(3, 64, 64).transpose(1, 2, 0)+1
+    plt.imshow(the_state*128)
     plt.show(block=False)
 
 
