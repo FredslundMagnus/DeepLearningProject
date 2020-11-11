c7ce60e38759a72369b378e3c0ef492b41204047
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-11-11 06:34:02 +0100

    Created Collector

--- Utils/server.py
+++ Utils/server.py
@@ -1,4 +1,5 @@
 
+from collector import Collector
 from Utils.debug import checkServer, getvals, profilingStats, showParams
 import pickle
 from agent import Agent
@@ -14,6 +15,7 @@ defaults = {
     'update_every': 100,
     'use_distribution': 1,
     'double': 1,
+    'total_agents': 20,
 }
 
 params = getvals(defaults) if isServer else None
@@ -34,8 +36,8 @@ def saveAgent(agent: Agent, name: str):
         pickle.dump(agent, open(f"trainlocally/{'-'.join(name.split('-')[:-1])}/{name}", "wb"))
 
 
-def saveMean(mean: List[float], name: str):
+def saveCollector(collector: Collector, name: str):
     if isServer:
-        pickle.dump(mean, open(f"outputs/{'-'.join(name.split('-')[:-1])}/Means/{name}.means", "wb"))
+        pickle.dump(collector, open(f"outputs/{'-'.join(name.split('-')[:-1])}/Collectors/{name}.collect", "wb"))
     else:
-        pickle.dump(mean, open(f"trainlocally/{'-'.join(name.split('-')[:-1])}/mean_{name}", "wb"))
+        pickle.dump(collector, open(f"trainlocally/{'-'.join(name.split('-')[:-1])}/collect_{name}", "wb"))
---
+++ collector.py
@@ -0,0 +1,17 @@
+class Collector:
+    def __init__(self, calculate_every: int = 50, total_agents: int = 20) -> None:
+        self.calculate_every, self.total_agents = calculate_every, total_agents
+        self.all_return, self.all_dones = [], []
+        self.dones, self.total_rew, self.k, self.f = 0, 0, 0, 0
+
+    def collect(self, rew, done, act):
+        self.f += 1
+        self.total_rew += sum(rew) / len(rew)
+        self.dones += sum(done) / len(done)
+
+        if self.f % self.calculate_every == 0:
+            self.k += 1
+            if self.dones != 0:
+                self.all_dones.append(self.k * self.calculate_every / self.dones)
+                self.all_return.append(self.total_rew / self.dones)
+                self.dones, self.total_rew, self.k = 0, 0, 0
