9fcae2a8954746a39b08ee3f6e602c60f2f66e4c
Magnus Fredslund <fredslundmagnus@gmail.com>
2020-11-27 22:41:46 +0100

    Fix

---
+++ maze_tests.py
@@ -0,0 +1,31 @@
+from collector import Collector
+from environments import Environments
+from Utils.server import isServer, params, serverRun, saveAgent, saveCollector
+from agent import Agent
+from display_input import displayer
+from Utils.debug import disablePrint
+from time import time
+from torch.nn import Module, Conv2d, MaxPool2d, Linear, MSELoss, LSTM, LeakyReLU, Sequential, ReLU
+from helpers import hidden_size
+from pynput import keyboard
+from display_input import showFilters, returnplot
+import matplotlib.pyplot as plt
+
+
+total_agents, display_every = 20, 5000
+agent = Agent(memory=40000, discount=0.995, uncertainty=False, update_every=100, double=True, use_distribution=False, reward_normalization=False)
+env = Environments(render=True, envs=['fruitbot' for _ in range(total_agents)], agent=agent)
+collector = Collector(calculate_every=500, total_agents=total_agents)
+for f in range(1, 10000000):
+    obs, hn, cn = env.start()
+    act, obs_old, h0, c0, hn, cn = agent.chooseMulti(obs, hn, cn)
+    obs, rew, done, info = env.step(act, hn, cn)
+    collector.collect(rew, done, act)
+    agent.rememberMulti(obs_old, act, obs, rew, h0, c0, hn, cn, done)
+    agent.learn()
+
+    if showPrint:
+        plt.close('all')
+        showFilters(obs[0], y=200, x=600)
+        returnplot(collector.all_return, x=1200, y=200)
+        showPrint = False
