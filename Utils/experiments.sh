50ec3cdd49c99370771a70308e798a436a7d3882
TheDingodile <TheDingodile@gmail.com>
2020-12-02 12:08:53 +0100

    plz_work2

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mkdir ../outputs/Final_stateUncertainty0and)bigfish/
-mkdir ../outputs/Final_stateUncertainty0and)bigfish/Markdown
-mkdir ../outputs/Final_stateUncertainty0and)bigfish/Agents
-mkdir ../outputs/Final_stateUncertainty0and)bigfish/Collectors
-bsub -o "../outputs/Final_stateUncertainty0and)bigfish/Markdown/Final_stateUncertainty0and)bigfish_0.md" -J "Final_stateUncertainty0and)bigfish_0" -P "Final_stateUncertainty0and)bigfish-0 -environment bigfish -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/Final_stateUncertainty0and0bigfish/
+mkdir ../outputs/Final_stateUncertainty0and0bigfish/Markdown
+mkdir ../outputs/Final_stateUncertainty0and0bigfish/Agents
+mkdir ../outputs/Final_stateUncertainty0and0bigfish/Collectors
+bsub -o "../outputs/Final_stateUncertainty0and0bigfish/Markdown/Final_stateUncertainty0and0bigfish_0.md" -J "Final_stateUncertainty0and0bigfish_0" -P "Final_stateUncertainty0and0bigfish-0 -environment bigfish -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0" < submit.sh
 mkdir ../outputs/Final_stateUncertainty0.25and0bigfish/
 mkdir ../outputs/Final_stateUncertainty0.25and0bigfish/Markdown
 mkdir ../outputs/Final_stateUncertainty0.25and0bigfish/Agents
--- Utils/generateExperiments.py
+++ Utils/generateExperiments.py
@@ -60,7 +60,7 @@ def genExperiments(name, n=1, **params):
 environments = ['bigfish']
 
 for env in environments:
-    genExperiments(f"Final_stateUncertainty0and){env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0)
+    genExperiments(f"Final_stateUncertainty0and0{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0)
     genExperiments(f"Final_stateUncertainty0.25and0{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.25, state_difference_weight=0)
     genExperiments(f"Final_stateUncertainty0and0.25{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.25)
     genExperiments(f"Final_stateUncertainty0.25and0.25{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.25, state_difference_weight=0.25)
