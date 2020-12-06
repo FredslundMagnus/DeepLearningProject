e667b92f4b0c9bd01214d5ca91fd3fcaaaf3c61a
TheDingodile <TheDingodile@gmail.com>
2020-12-06 01:17:05 +0100

    dodgeball_exps

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
-mkdir ../outputs/Uncertainty0state_difference0.1softepschaser/
-mkdir ../outputs/Uncertainty0state_difference0.1softepschaser/Markdown
-mkdir ../outputs/Uncertainty0state_difference0.1softepschaser/Agents
-mkdir ../outputs/Uncertainty0state_difference0.1softepschaser/Collectors
-bsub -o "../outputs/Uncertainty0state_difference0.1softepschaser/Markdown/Uncertainty0state_difference0.1softepschaser_0.md" -J "Uncertainty0state_difference0.1softepschaser_0" -P "Uncertainty0state_difference0.1softepschaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1 -exploration epsintosoftmax" < submit.sh
-mkdir ../outputs/Uncertainty0state_difference0.1chaser/
-mkdir ../outputs/Uncertainty0state_difference0.1chaser/Markdown
-mkdir ../outputs/Uncertainty0state_difference0.1chaser/Agents
-mkdir ../outputs/Uncertainty0state_difference0.1chaser/Collectors
-bsub -o "../outputs/Uncertainty0state_difference0.1chaser/Markdown/Uncertainty0state_difference0.1chaser_0.md" -J "Uncertainty0state_difference0.1chaser_0" -P "Uncertainty0state_difference0.1chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1" < submit.sh
-mkdir ../outputs/Uncertainty0.1state_difference0softepschaser/
-mkdir ../outputs/Uncertainty0.1state_difference0softepschaser/Markdown
-mkdir ../outputs/Uncertainty0.1state_difference0softepschaser/Agents
-mkdir ../outputs/Uncertainty0.1state_difference0softepschaser/Collectors
-bsub -o "../outputs/Uncertainty0.1state_difference0softepschaser/Markdown/Uncertainty0.1state_difference0softepschaser_0.md" -J "Uncertainty0.1state_difference0softepschaser_0" -P "Uncertainty0.1state_difference0softepschaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0 -exploration epsintosoftmax" < submit.sh
-mkdir ../outputs/Uncertainty0.1state_difference0chaser/
-mkdir ../outputs/Uncertainty0.1state_difference0chaser/Markdown
-mkdir ../outputs/Uncertainty0.1state_difference0chaser/Agents
-mkdir ../outputs/Uncertainty0.1state_difference0chaser/Collectors
-bsub -o "../outputs/Uncertainty0.1state_difference0chaser/Markdown/Uncertainty0.1state_difference0chaser_0.md" -J "Uncertainty0.1state_difference0chaser_0" -P "Uncertainty0.1state_difference0chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0" < submit.sh
-mkdir ../outputs/Uncertainty0state_difference0chaser/
-mkdir ../outputs/Uncertainty0state_difference0chaser/Markdown
-mkdir ../outputs/Uncertainty0state_difference0chaser/Agents
-mkdir ../outputs/Uncertainty0state_difference0chaser/Collectors
-bsub -o "../outputs/Uncertainty0state_difference0chaser/Markdown/Uncertainty0state_difference0chaser_0.md" -J "Uncertainty0state_difference0chaser_0" -P "Uncertainty0state_difference0chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0" < submit.sh
-mkdir ../outputs/Uncertainty0state_difference0chaser/
-mkdir ../outputs/Uncertainty0state_difference0chaser/Markdown
-mkdir ../outputs/Uncertainty0state_difference0chaser/Agents
-mkdir ../outputs/Uncertainty0state_difference0chaser/Collectors
-bsub -o "../outputs/Uncertainty0state_difference0chaser/Markdown/Uncertainty0state_difference0chaser_0.md" -J "Uncertainty0state_difference0chaser_0" -P "Uncertainty0state_difference0chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0 -exploration epsintosoftmax" < submit.sh
+mkdir ../outputs/Uncertainty0state_difference0.1softepsdodgeball/
+mkdir ../outputs/Uncertainty0state_difference0.1softepsdodgeball/Markdown
+mkdir ../outputs/Uncertainty0state_difference0.1softepsdodgeball/Agents
+mkdir ../outputs/Uncertainty0state_difference0.1softepsdodgeball/Collectors
+bsub -o "../outputs/Uncertainty0state_difference0.1softepsdodgeball/Markdown/Uncertainty0state_difference0.1softepsdodgeball_0.md" -J "Uncertainty0state_difference0.1softepsdodgeball_0" -P "Uncertainty0state_difference0.1softepsdodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1 -exploration epsintosoftmax" < submit.sh
+mkdir ../outputs/Uncertainty0state_difference0.1dodgeball/
+mkdir ../outputs/Uncertainty0state_difference0.1dodgeball/Markdown
+mkdir ../outputs/Uncertainty0state_difference0.1dodgeball/Agents
+mkdir ../outputs/Uncertainty0state_difference0.1dodgeball/Collectors
+bsub -o "../outputs/Uncertainty0state_difference0.1dodgeball/Markdown/Uncertainty0state_difference0.1dodgeball_0.md" -J "Uncertainty0state_difference0.1dodgeball_0" -P "Uncertainty0state_difference0.1dodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1" < submit.sh
+mkdir ../outputs/Uncertainty0.1state_difference0softepsdodgeball/
+mkdir ../outputs/Uncertainty0.1state_difference0softepsdodgeball/Markdown
+mkdir ../outputs/Uncertainty0.1state_difference0softepsdodgeball/Agents
+mkdir ../outputs/Uncertainty0.1state_difference0softepsdodgeball/Collectors
+bsub -o "../outputs/Uncertainty0.1state_difference0softepsdodgeball/Markdown/Uncertainty0.1state_difference0softepsdodgeball_0.md" -J "Uncertainty0.1state_difference0softepsdodgeball_0" -P "Uncertainty0.1state_difference0softepsdodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0 -exploration epsintosoftmax" < submit.sh
+mkdir ../outputs/Uncertainty0.1state_difference0dodgeball/
+mkdir ../outputs/Uncertainty0.1state_difference0dodgeball/Markdown
+mkdir ../outputs/Uncertainty0.1state_difference0dodgeball/Agents
+mkdir ../outputs/Uncertainty0.1state_difference0dodgeball/Collectors
+bsub -o "../outputs/Uncertainty0.1state_difference0dodgeball/Markdown/Uncertainty0.1state_difference0dodgeball_0.md" -J "Uncertainty0.1state_difference0dodgeball_0" -P "Uncertainty0.1state_difference0dodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/Uncertainty0state_difference0dodgeball/
+mkdir ../outputs/Uncertainty0state_difference0dodgeball/Markdown
+mkdir ../outputs/Uncertainty0state_difference0dodgeball/Agents
+mkdir ../outputs/Uncertainty0state_difference0dodgeball/Collectors
+bsub -o "../outputs/Uncertainty0state_difference0dodgeball/Markdown/Uncertainty0state_difference0dodgeball_0.md" -J "Uncertainty0state_difference0dodgeball_0" -P "Uncertainty0state_difference0dodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/Uncertainty0state_difference0softepsdodgeball/
+mkdir ../outputs/Uncertainty0state_difference0softepsdodgeball/Markdown
+mkdir ../outputs/Uncertainty0state_difference0softepsdodgeball/Agents
+mkdir ../outputs/Uncertainty0state_difference0softepsdodgeball/Collectors
+bsub -o "../outputs/Uncertainty0state_difference0softepsdodgeball/Markdown/Uncertainty0state_difference0softepsdodgeball_0.md" -J "Uncertainty0state_difference0softepsdodgeball_0" -P "Uncertainty0state_difference0softepsdodgeball-0 -environment dodgeball -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0 -exploration epsintosoftmax" < submit.sh
