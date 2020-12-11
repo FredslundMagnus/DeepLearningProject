a34eb70751bd368b52a9dd4df8a6704839131bfc
TheDingodile <TheDingodile@gmail.com>
2020-12-11 15:07:44 +0100

    CHASER_ALL_TESTS

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,16 +1,56 @@
 #!/bin/sh
-mkdir ../outputs/CHASER_U_S_0_0chaser/
-mkdir ../outputs/CHASER_U_S_0_0chaser/Markdown
-mkdir ../outputs/CHASER_U_S_0_0chaser/Agents
-mkdir ../outputs/CHASER_U_S_0_0chaser/Collectors
-bsub -o "../outputs/CHASER_U_S_0_0chaser/Markdown/CHASER_U_S_0_0chaser_0.md" -J "CHASER_U_S_0_0chaser_0" -P "CHASER_U_S_0_0chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0" < submit.sh
-mkdir ../outputs/CHASER_U_S_0.1_0chaser/
-mkdir ../outputs/CHASER_U_S_0.1_0chaser/Markdown
-mkdir ../outputs/CHASER_U_S_0.1_0chaser/Agents
-mkdir ../outputs/CHASER_U_S_0.1_0chaser/Collectors
-bsub -o "../outputs/CHASER_U_S_0.1_0chaser/Markdown/CHASER_U_S_0.1_0chaser_0.md" -J "CHASER_U_S_0.1_0chaser_0" -P "CHASER_U_S_0.1_0chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0" < submit.sh
-mkdir ../outputs/CHASER_U_S_0_0.1chaser/
-mkdir ../outputs/CHASER_U_S_0_0.1chaser/Markdown
-mkdir ../outputs/CHASER_U_S_0_0.1chaser/Agents
-mkdir ../outputs/CHASER_U_S_0_0.1chaser/Collectors
-bsub -o "../outputs/CHASER_U_S_0_0.1chaser/Markdown/CHASER_U_S_0_0.1chaser_0.md" -J "CHASER_U_S_0_0.1chaser_0" -P "CHASER_U_S_0_0.1chaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.1_0returnchaser/
+mkdir ../outputs/CHASER_U_S_0.1_0returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.1_0returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.1_0returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.1_0returnchaser/Markdown/CHASER_U_S_0.1_0returnchaser_0.md" -J "CHASER_U_S_0.1_0returnchaser_0" -P "CHASER_U_S_0.1_0returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.1 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/CHASER_U_S_0_0.1returnchaser/
+mkdir ../outputs/CHASER_U_S_0_0.1returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0_0.1returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0_0.1returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0_0.1returnchaser/Markdown/CHASER_U_S_0_0.1returnchaser_0.md" -J "CHASER_U_S_0_0.1returnchaser_0" -P "CHASER_U_S_0_0.1returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.1" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.2_0returnchaser/
+mkdir ../outputs/CHASER_U_S_0.2_0returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.2_0returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.2_0returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.2_0returnchaser/Markdown/CHASER_U_S_0.2_0returnchaser_0.md" -J "CHASER_U_S_0.2_0returnchaser_0" -P "CHASER_U_S_0.2_0returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.2 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/CHASER_U_S_0_0.2returnchaser/
+mkdir ../outputs/CHASER_U_S_0_0.2returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0_0.2returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0_0.2returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0_0.2returnchaser/Markdown/CHASER_U_S_0_0.2returnchaser_0.md" -J "CHASER_U_S_0_0.2returnchaser_0" -P "CHASER_U_S_0_0.2returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.2" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.01_0returnchaser/
+mkdir ../outputs/CHASER_U_S_0.01_0returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.01_0returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.01_0returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.01_0returnchaser/Markdown/CHASER_U_S_0.01_0returnchaser_0.md" -J "CHASER_U_S_0.01_0returnchaser_0" -P "CHASER_U_S_0.01_0returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.01 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/CHASER_U_S_0_0.01returnchaser/
+mkdir ../outputs/CHASER_U_S_0_0.01returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0_0.01returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0_0.01returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0_0.01returnchaser/Markdown/CHASER_U_S_0_0.01returnchaser_0.md" -J "CHASER_U_S_0_0.01returnchaser_0" -P "CHASER_U_S_0_0.01returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.01" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.05_0returnchaser/
+mkdir ../outputs/CHASER_U_S_0.05_0returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.05_0returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.05_0returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.05_0returnchaser/Markdown/CHASER_U_S_0.05_0returnchaser_0.md" -J "CHASER_U_S_0.05_0returnchaser_0" -P "CHASER_U_S_0.05_0returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.05 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/CHASER_U_S_0_0.05returnchaser/
+mkdir ../outputs/CHASER_U_S_0_0.05returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0_0.05returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0_0.05returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0_0.05returnchaser/Markdown/CHASER_U_S_0_0.05returnchaser_0.md" -J "CHASER_U_S_0_0.05returnchaser_0" -P "CHASER_U_S_0_0.05returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.05" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.01_0.01returnchaser/
+mkdir ../outputs/CHASER_U_S_0.01_0.01returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.01_0.01returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.01_0.01returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.01_0.01returnchaser/Markdown/CHASER_U_S_0.01_0.01returnchaser_0.md" -J "CHASER_U_S_0.01_0.01returnchaser_0" -P "CHASER_U_S_0.01_0.01returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.01 -state_difference_weight 0.01" < submit.sh
+mkdir ../outputs/CHASER_U_S_0.001_0returnchaser/
+mkdir ../outputs/CHASER_U_S_0.001_0returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0.001_0returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0.001_0returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0.001_0returnchaser/Markdown/CHASER_U_S_0.001_0returnchaser_0.md" -J "CHASER_U_S_0.001_0returnchaser_0" -P "CHASER_U_S_0.001_0returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0.001 -state_difference_weight 0" < submit.sh
+mkdir ../outputs/CHASER_U_S_0_0.001returnchaser/
+mkdir ../outputs/CHASER_U_S_0_0.001returnchaser/Markdown
+mkdir ../outputs/CHASER_U_S_0_0.001returnchaser/Agents
+mkdir ../outputs/CHASER_U_S_0_0.001returnchaser/Collectors
+bsub -o "../outputs/CHASER_U_S_0_0.001returnchaser/Markdown/CHASER_U_S_0_0.001returnchaser_0.md" -J "CHASER_U_S_0_0.001returnchaser_0" -P "CHASER_U_S_0_0.001returnchaser-0 -environment chaser -uncertainty 1 -state_difference 1 -uncertainty_weight 0 -state_difference_weight 0.001" < submit.sh
--- Utils/generateExperiments.py
+++ Utils/generateExperiments.py
@@ -65,9 +65,17 @@ def genExperiments(name, n=1, **params):
 environments = ['chaser']
 
 for env in environments:
-    genExperiments(f"CHASER_U_S_0_0{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0)
-    genExperiments(f"CHASER_U_S_0.1_0{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.1, state_difference_weight=0)
-    genExperiments(f"CHASER_U_S_0_0.1{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.1)
+    genExperiments(f"CHASER_U_S_0.1_0return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.1, state_difference_weight=0)
+    genExperiments(f"CHASER_U_S_0_0.1return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.1)
+    genExperiments(f"CHASER_U_S_0.2_0return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.2, state_difference_weight=0)
+    genExperiments(f"CHASER_U_S_0_0.2return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.2)
+    genExperiments(f"CHASER_U_S_0.01_0return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.01, state_difference_weight=0)
+    genExperiments(f"CHASER_U_S_0_0.01return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.01)
+    genExperiments(f"CHASER_U_S_0.05_0return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.05, state_difference_weight=0)
+    genExperiments(f"CHASER_U_S_0_0.05return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.05)
+    genExperiments(f"CHASER_U_S_0.01_0.01return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.01, state_difference_weight=0.01)
+    genExperiments(f"CHASER_U_S_0.001_0return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0.001, state_difference_weight=0)
+    genExperiments(f"CHASER_U_S_0_0.001return{env}", environment=env, uncertainty=1, state_difference=1, uncertainty_weight=0, state_difference_weight=0.001)
 
 
 file.close()
